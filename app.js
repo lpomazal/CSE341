7e0474c57732f70719775b181df5c76edade2900
lpomazal <lpomazal@byui.edu>
2022-01-07 00:25:43 -0800

    assignment1

--- app.js
+++ app.js
@@ -1,42 +1,40 @@
 const http = require('http');
-const fs = require('fs');
-const { resetWatchers } = require('nodemon/lib/monitor/watch');
 
+//const routes = require('.\\routes');
 
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+const server = http.createServer((req, res) =>{
     const url = req.url;
-    const method = req.method;
-    if (url === '/'){
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><form action="/create-user" method="post"><input tupe="text" name="username"><button type="submit"><button>Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
     }
-    if (url === '/message' && method === 'POST'){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
+    if (url === '/users'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Assignment 1</title></head>');
+        res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    // Html response "page not found"
+    if (url === '/create-user'){
+        const body =[];
+        req.on('data', chunk =>{
             body.push(chunk);
         });
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });  
+            console.log(parsedBody.split('=')[1]); // username= whatever is inputed
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
-        return res.end();
+        res.end();
     }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
 });
 
 server.listen(4000);
+//on 4000 because my computer is using 3000 for itself
