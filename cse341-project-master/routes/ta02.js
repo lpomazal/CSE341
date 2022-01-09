0ee9d845870f2ee4906a3063579d6f0e53894fb0
lpomazal <lpomazal@byui.edu>
2022-01-08 19:47:42 -0800

    week 2 team activity

--- cse341-project-master/routes/ta02.js
+++ cse341-project-master/routes/ta02.js
@@ -2,6 +2,28 @@
 // Remember, you can make more of these placeholders yourself!
 const express = require('express');
 const router = express.Router();
+//requirement 01
+const userArray =['Jack', 'Jill', 'Brian'];
+
+//requirement 02
+router.post('/addUser', (req, res, next) => {
+  const newUser = req.body.newUser:
+
+  userArray.push(newUser);
+
+  res.redirect('/ta02/');
+});
+
+//requirement 03
+router.post('/removeUser', (req, res, next) => {
+  const remUser = req.body.remUser;
+  //removes from array
+  const index = userArray.indexOf(remUser);
+  if (index !== -1){
+    userArray.splice(index, 1);
+  }
+  res.redirect('/ta02/');
+});
 
 router.get('/', (req, res, next) => {
   res.render('pages', {
