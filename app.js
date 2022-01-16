876e29acd67b5ac1c5dfdd6d2c5beb008f5ac9a1
lpomazal <lpomazal@byui.edu>
2022-01-15 22:35:54 -0800

    week4

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 const bodyParser = require('body-parser');
 const app = express();
 
@@ -17,3 +18,31 @@ app.use((req, res, next) =>{
 });
 
 app.listen(4000);
+
+const corsOptions = {
+    origin: "https://lpomazalcse341.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const options = {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    family: 4
+};
+
+mongoose
+  .connect(
+    MONGODB_URL, options
+  )
+  .then(result => {
+     // This should be your user handling code implement following the course videos
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://lpomazal:school2022@cse341cluster-3dwlw.mongodb.net/test?retryWrites=true&w=majority";
